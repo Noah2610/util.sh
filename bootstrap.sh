e970d23cd9501f2d30788fb087e1c8cd67f10f8b
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-07-17 03:42:39 +0200

    Added comment about the shellcheck source warning

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,8 @@
 #   curl "https://raw.githubusercontent.com/Noah2610/util.sh/master/download-util.sh" > download-util.sh
 # Put the code below at the beginning of your bash script:
 
+# shellcheck source=./util.sh  # Optional; fixes shellcheck's `source` warning
 _dir="$( dirname "$0" )"
 [ -f "${_dir}/util.sh" ] || "${_dir}/download-util.sh" || exit 1
-# shellcheck source=./util.sh
 source "${_dir}/util.sh"
 unset _dir
