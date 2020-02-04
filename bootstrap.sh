187b74aaab5f30fb385f805a1ac29b4f9cb82dc9
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-02-04 14:16:33 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,13 @@
 # Run the following inside the directory of your bash script:
-#   curl "https://raw.githubusercontent.com/Noah2610/util.sh/master/download-util.sh" > download-util.sh
-# Put the code below at the beginning of your bash script:
+#   curl "https://raw.githubusercontent.com/Noah2610/util.sh/v2/download-util.sh" > download-util.sh
+# Place the code below at the beginning of your bash script.
+# Set the `UTIL_VERSION` variable to a different git reference, to specify a custom version.
+# The version can be a branch, a tag, or a git hash.
 
-# shellcheck source=./util.sh  # Optional; fixes shellcheck's `source` warning
-_dir="$( dirname "$0" )"
-[ -f "${_dir}/util.sh" ] || bash "${_dir}/download-util.sh" || exit 1
-source "${_dir}/util.sh"
-unset _dir
+# shellcheck source=./util.sh disable=SC2155
+function _dl_util_sh {
+    local UTIL_VERSION="v2"
+    local dir="$( dirname "$1" )"
+    [ -f "${dir}/util.sh" ] || bash "${dir}/download-util.sh" "$UTIL_VERSION" || exit 1
+    source "${dir}/util.sh"
+}; _dl_util_sh "$0"
