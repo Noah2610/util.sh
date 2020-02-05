a0943597b3e3fb377f95ae7b94be63c843011d99
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-02-05 01:25:47 +0100

    Add $DOC_COMMENT variable; also print doc comments if no associated code is given

--- DOCS.md
+++ DOCS.md
@@ -1,33 +1,45 @@
+util.sh  
+Version: 2.0.0  
+https://github.com/Noah2610/util.sh
+
+---
+
 ### `function is_available`
+  
 Returns `0` or `1` depending on if the given string is available as a command.
 
 ### `function is_running`
+  
 Returns `0` or `1` depending on if the command is running (using `pgrep`).
 
 ### `function check`
+  
 Exit with error message if the given string is not available as a command.
 
 ### `function check_file`
+  
 Exit with error message if the given file path does not exist.
 
 ### `function check_dir`
+  
 Exit with error message if the given directory path does not exist.
 
 ### `function msg`
+  
 Print the given string to stdout.
 
 ### `function err`
+  
 Print the given string to stderr and to the `$LOGFILE` (if one exists),  
 then exit with exit code 1.
 
 ### `function warn`
+  
 Print the given string to stderr and to the `$LOGFILE`
 
 ### `function print_log`
+  
 Print the given string to the `$LOGFILE` (if one exists), and strip color from the text.
-
-### `function _strip_ansi_codes`
-Strip ansi codes from the given string.
 
 ### `function clrfg`
 Echos the _FOREGROUND_ color code matching the given color argument.  
@@ -39,10 +51,10 @@ Available colors:
 "lightred", "lightgreen", "lightblue", "lightyellow",  
 "lightmagenta", "lightcyan",  
 "default", "reset"  
-```  
-https://misc.flogisoft.com/bash/tip_colors_and_formatting
+```
 
 ### `function clrbg`
+  
 Echos the _BACKGROUND_ color code matching the given color argument.  
 The given color string is a semantic representation of color.  
 Available colors:  
@@ -55,6 +67,7 @@ Available colors:
 ```
 
 ### `function clrattr`
+  
 Echos the attribute color code matching the given argument.  
 The given argument string is a semantic representation of an attribute.  
 Available attributes:  
@@ -64,6 +77,7 @@ Available attributes:
 ```
 
 ### `function clr`
+  
 Set the foreground, background, and attribute colors all at once.  
 This function takes three arguments:  
 - foreground color (see `clrfg`)  
@@ -71,20 +85,18 @@ This function takes three arguments:
 - attribute (see `clrattr`)
 
 ### `function clrrs`
+  
 Resets all color settings to default.
 
-### `function _color_code_generic`
-Internal helper function.  
-Returns the foreground color code number for the given color string.  
-This is then used in the above color functions to create  
-proper, printable color codes for foreground and background.
-
 ### `function semantic_date`
+  
+__TODO:__ Refactor?  
 Print out a date string in a specifc format.  
 If the command `boxed-string` is available, then it calls that with the date string.  
 boxed-string: https://gist.github.com/Noah2610/2c4a92f6732419becade2f76bc943039
 
 ### `function try_run`
+  
 Tries to run the given command.  
 All arguments are parsed as the command, so the first argument is the  
 command, and all following arguments are passed as arguments to the command.  
@@ -92,6 +104,7 @@ Runs `err` if the command fails.
 Writes the command's output to the `$LOGFILE`.
 
 ### `function try_run_hidden`
+  
 Similar to function `try_run_hidden`, but hides the command's output.  
 Tries to run the given command and hides its output.  
 All arguments are parsed as the command, so the first argument is the  
@@ -100,17 +113,21 @@ If the command fails, then it prints the output with `err`.
 Writes the command's output to the `$LOGFILE`.
 
 ### `function is_positive`
+  
 Returns `0` if the given argument represents a "positive" value (not empty and non-0).
 
 ### `function should_run_in_terminal`
+  
 Returns `0` or `1` depending on if the final command should be run in a new terminal.  
 For very specific use-case(s).
 
 ### `function run_terminal`
+  
 Run the given command in a new terminal.  
 The first argument is the command, any following arguments  
 are passed to the command as its arguments.  
 The new shell's working directory is set to the `$ROOT` variable.
 
 ### `function join_by`
+  
 https://stackoverflow.com/a/17841619
--- util.sh
+++ util.sh
@@ -1,17 +1,17 @@
-# util.sh
-# Version: 2.0.0-pre
-# https://github.com/Noah2610/util.sh
+## util.sh
+## Version: 2.0.0
+## https://github.com/Noah2610/util.sh
 
 set -o pipefail
 
-# Returns `0` or `1` depending on if the given string is available as a command.
+## Returns `0` or `1` depending on if the given string is available as a command.
 function is_available {
     local cmd="$1"
     [ -z "$cmd" ] && err "No command to check for availability given to function \`is_available\`"
     command -v "$cmd" &> /dev/null
 }
 
-# Returns `0` or `1` depending on if the command is running (using `pgrep`).
+## Returns `0` or `1` depending on if the command is running (using `pgrep`).
 function is_running {
     check "pgrep"
     local to_check="$1"
@@ -19,7 +19,7 @@ function is_running {
     pgrep -xc "$to_check" &> /dev/null
 }
 
-# Exit with error message if the given string is not available as a command.
+## Exit with error message if the given string is not available as a command.
 function check {
     local cmd="$1"
     [ -z "$cmd" ] && err "No command to check given to function \`check\`"
@@ -27,49 +27,41 @@ function check {
         || err "\`$(clr "${CLR_CODE[@]}")${cmd}$(clrrs)\` is not available."
 }
 
-# Exit with error message if the given file path does not exist.
+## Exit with error message if the given file path does not exist.
 function check_file {
     local FILE="$1"
     [ -z "$FILE" ] && err "No file given"
     [ -f "$FILE" ] || err "File does not exist: $(clr "${CLR_CODE[@]}")${FILE}$(clrrs)"
 }
 
-# Exit with error message if the given directory path does not exist.
+## Exit with error message if the given directory path does not exist.
 function check_dir {
     local DIR="$1"
     [ -z "$FILE" ] && err "No directory given"
     [ -d "$DIR" ] || err "Directory does not exist: $(clr "${CLR_CODE[@]}")${DIR}$(clrrs)"
 }
 
-# Print the given string to stdout.
+## Print the given string to stdout.
 function msg {
     print_log "$1"
     echo -e "$(clr "${CLR_MSG[@]}")${1}$(clrrs)"
 }
 
-# DEPRECATED
-# Same as `msg`, but also makes the text bold.
-function msg_strong {
-    warn "Function \`msg_strong\` is deprecated"
-    print_log "$( semantic_date )\n${1}"
-    echo -e "$( colored "${COLOR_MSG_STRONG}" "${1}" )"
-}
-
-# Print the given string to stderr and to the `$LOGFILE` (if one exists),
-# then exit with exit code 1.
+## Print the given string to stderr and to the `$LOGFILE` (if one exists),
+## then exit with exit code 1.
 function err {
     print_log "$( semantic_date )\nERROR: $1"
     (1>&2 echo -e "$(clr "${CLR_ERR[@]}")ERROR:$(clrrs) ${1}\nExiting.")
     exit 1
 }
 
-# Print the given string to stderr and to the `$LOGFILE`
+## Print the given string to stderr and to the `$LOGFILE`
 function warn {
     print_log "WARNING: $1"
     (1>&2 echo -e "$(clr "${CLR_WARN[@]}")WARNING:$(clrrs) ${1}")
 }
 
-# Print the given string to the `$LOGFILE` (if one exists), and strip color from the text.
+## Print the given string to the `$LOGFILE` (if one exists), and strip color from the text.
 function print_log {
     local txt="$1"
     [ -z "$txt" ] && err "No message text given to function \`print_log\`"
@@ -86,15 +78,17 @@ function _strip_ansi_codes {
     fi
 }
 
-# Echos the FOREGROUND color code matching the given color argument.
-# The given color string is a semantic representation of color.
-# Available colors:
-#   "black", "white", "red", "green", "blue",
-#   "yellow", "magenta", "cyan", "lightgray", "darkgray",
-#   "lightred", "lightgreen", "lightblue", "lightyellow",
-#   "lightmagenta", "lightcyan",
-#   "default", "reset"
 # https://misc.flogisoft.com/bash/tip_colors_and_formatting
+## Echos the _FOREGROUND_ color code matching the given color argument.
+## The given color string is a semantic representation of color.
+## Available colors:
+## ```
+##   "black", "white", "red", "green", "blue",
+##   "yellow", "magenta", "cyan", "lightgray", "darkgray",
+##   "lightred", "lightgreen", "lightblue", "lightyellow",
+##   "lightmagenta", "lightcyan",
+##   "default", "reset"
+## ```
 function clrfg {
     local color_str="$1"
     local color_code
@@ -103,14 +97,16 @@ function clrfg {
     echo -en "\e[${color_code}m"
 }
 
-# Echos the BACKGROUND color code matching the given color argument.
-# The given color string is a semantic representation of color.
-# Available colors:
-#   "black", "white", "red", "green", "blue",
-#   "yellow", "magenta", "cyan", "lightgray", "darkgray",
-#   "lightred", "lightgreen", "lightblue", "lightyellow",
-#   "lightmagenta", "lightcyan",
-#   "default", "reset"
+## Echos the _BACKGROUND_ color code matching the given color argument.
+## The given color string is a semantic representation of color.
+## Available colors:
+## ```
+##   "black", "white", "red", "green", "blue",
+##   "yellow", "magenta", "cyan", "lightgray", "darkgray",
+##   "lightred", "lightgreen", "lightblue", "lightyellow",
+##   "lightmagenta", "lightcyan",
+##   "default", "reset"
+## ```
 function clrbg {
     local color_str="$1"
     local color_code
@@ -119,11 +115,13 @@ function clrbg {
     echo -en "\e[${color_code}m"
 }
 
-# Echos the attribute color code matching the given argument.
-# The given argument string is a semantic representation of an attribute.
-# Available attributes:
-#   "bold", "dim", "underline", "blink", "invert", "hidden",
-#   "default", "reset"
+## Echos the attribute color code matching the given argument.
+## The given argument string is a semantic representation of an attribute.
+## Available attributes:
+## ```
+##   "bold", "dim", "underline", "blink", "invert", "hidden",
+##   "default", "reset"
+## ```
 function clrattr {
     local attr_str="$1"
     [ -z "$attr_str" ] && err "No attribute argument given to function \`clrattr\`"
@@ -140,11 +138,11 @@ function clrattr {
     esac
 }
 
-# Set the foreground, background, and attribute colors all at once.
-# This function takes three arguments:
-#   foreground color (see clrfg)
-#   background color (see clrbg)
-#   attribute (see clrattr)
+## Set the foreground, background, and attribute colors all at once.
+## This function takes three arguments:
+## - foreground color (see `clrfg`)
+## - background color (see `clrbg`)
+## - attribute (see `clrattr`)
 function clr {
     local color_fg="$1"
     local color_bg="$2"
@@ -157,7 +155,7 @@ function clr {
     clrattr "$color_attr"
 }
 
-# Resets all color settings to default.
+## Resets all color settings to default.
 function clrrs {
     echo -en "\e[0m"
 }
@@ -192,23 +190,10 @@ function _color_code_generic {
     esac
 }
 
-# DEPRECATED
-# This function returns a string wrapped in ansi color codes.
-# It takes two arguments:
-#   color - ansi color code (ex. "0;33", "0;36;40")
-#   txt   - the text to be colored
-function colored {
-    warn "Function \`colored\` is deprecated"
-    local color="$1"
-    local txt="$2"
-    { [ -z "$color" ] || [ -z "$txt" ]; } \
-        && err "Function \`colored\` needs two arguments: the color and the text."
-    echo "\033[${color}m${txt}\033[m"
-}
-
-# Print out a date string in a specifc format.
-# If the command `boxed-string` is available, then it calls that with the date string.
-# boxed-string: https://gist.github.com/Noah2610/2c4a92f6732419becade2f76bc943039
+## __TODO:__ Refactor?
+## Print out a date string in a specifc format.
+## If the command `boxed-string` is available, then it calls that with the date string.
+## boxed-string: https://gist.github.com/Noah2610/2c4a92f6732419becade2f76bc943039
 function semantic_date {
     check "date"
     local dfmt
@@ -224,11 +209,11 @@ function semantic_date {
     fi
 }
 
-# Tries to run the given command.
-# All arguments are parsed as the command, so the first argument is the
-# command, and all following arguments are passed as arguments to the command.
-# Runs `err` if the command fails.
-# Writes the command's output to the `$LOGFILE`.
+## Tries to run the given command.
+## All arguments are parsed as the command, so the first argument is the
+## command, and all following arguments are passed as arguments to the command.
+## Runs `err` if the command fails.
+## Writes the command's output to the `$LOGFILE`.
 function try_run {
     local cmd=( "$@" )
     [ ${#cmd} -lt 1 ] && err "No command given to function \`try_run\`"
@@ -243,12 +228,12 @@ function try_run {
     fi
 }
 
-# Similar to function `try_run_hidden`, but hides the command's output.
-# Tries to run the given command and hides its output.
-# All arguments are parsed as the command, so the first argument is the
-# command, and all following arguments are passed as arguments to the command.
-# If the command fails, then it prints the output with `err`.
-# Writes the command's output to the `$LOGFILE`.
+## Similar to function `try_run_hidden`, but hides the command's output.
+## Tries to run the given command and hides its output.
+## All arguments are parsed as the command, so the first argument is the
+## command, and all following arguments are passed as arguments to the command.
+## If the command fails, then it prints the output with `err`.
+## Writes the command's output to the `$LOGFILE`.
 function try_run_hidden {
     local cmd=( "$@" )
     [ ${#cmd} -gt 0 ] || err "No command given to function \`try_run_hidden\`"
@@ -266,23 +251,23 @@ ${out}"
     fi
 }
 
-# Returns `0` if the given argument represents a "positive" value (not empty and non-0).
+## Returns `0` if the given argument represents a "positive" value (not empty and non-0).
 function is_positive {
     [ -n "$1" ] && [ "$1" != "0" ]
 }
 
-# Returns `0` or `1` depending on if the final command should be run in a new terminal.
-# For very specific use-case(s).
+## Returns `0` or `1` depending on if the final command should be run in a new terminal.
+## For very specific use-case(s).
 function should_run_in_terminal {
     is_positive "$RUN_TERMINAL" \
         && [ -n "$TERMINAL" ] \
         && is_available "$TERMINAL"
 }
 
-# Run the given command in a new terminal.
-# The first argument is the command, any following arguments
-# are passed to the command as its arguments.
-# The new shell's working directory is set to the `$ROOT` variable.
+## Run the given command in a new terminal.
+## The first argument is the command, any following arguments
+## are passed to the command as its arguments.
+## The new shell's working directory is set to the `$ROOT` variable.
 function run_terminal {
     local cmd=( "$@" )
     [ ${#cmd} -gt 0 ] || err "No command given to function \`run_terminal\`."
@@ -300,7 +285,7 @@ function run_terminal {
     esac
 }
 
-# https://stackoverflow.com/a/17841619
+## https://stackoverflow.com/a/17841619
 function join_by {
     local d=$1
     shift
@@ -309,6 +294,8 @@ function join_by {
     printf "%s" "${@/#/$d}"
 }
 
+# Initialize the helper script.
+# Is run at the end of this script.
 function _init {
     check "basename"
     check "dirname"
@@ -337,13 +324,8 @@ function _init {
     # Set the `$TERMINAL` variable unless it was already set.
     [ -z "$TERMINAL" ] && TERMINAL="termite"
 
-    # DEPRECATED
-    # Set some ansi color code variables.
-    COLOR_ERR="1;31"
-    COLOR_MSG="0;33"
-    COLOR_MSG_STRONG="1;33"
-    COLOR_CODE="0;36;40"
-
+    # Set some commonly used colors.
+    # Arrays have the format of `( "fgcolor" "bgcolor" "attribute" )`.
     CLR_ERR=( "black" "red" "bold" )
     CLR_WARN=( "black" "yellow" "bold" )
     CLR_MSG=( "yellow" "default" "default" )
