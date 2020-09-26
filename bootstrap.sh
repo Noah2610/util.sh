13466c84fc96d63973e37286d2fc4002ec9d6ed4
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-09-26 03:40:36 +0200

    Add NO_COLOR variable
    When `$NO_COLOR` is set `clr*` functions don't do anything.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@
 
 # shellcheck source=./util.sh disable=SC2155
 function _dl_util_sh {
-    local UTIL_VERSION="v2.2.4"
+    local UTIL_VERSION="v2.2.5"
     local dir="$( dirname "$1" )"
     [ -f "${dir}/util.sh" ] || bash "${dir}/download-util.sh" "$UTIL_VERSION" || exit 1
     source "${dir}/util.sh"
